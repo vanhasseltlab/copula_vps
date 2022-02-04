f4d16737a84d07909a5cec1cfc759fa895f7f180
laurazwep <l.b.zwep@lacdr.leidenuniv.nl>
2022-02-04 11:04:47 +0100

    Moved plots from longitudinal_copulas to longitudinal_copulas_plots. Added code for multiple longitudinal variables at once.

--- scripts/longitudinal_copulas.R
+++ scripts/longitudinal_copulas.R
@@ -14,21 +14,6 @@ data_pregnancy_raw <- read.csv("data/Jig - pregnancy file August 2012 minus coag
                                row.names = NULL, na.strings = c("")) %>% 
   mutate(Neutrophils = as.numeric(str_replace(Neutrophils, ":", ".")))
 
-#explore some patients
-data_head <- data_pregnancy_raw %>% filter(ID %in% 1:5)
-data_head %>% 
-  ggplot(aes(x = Platelets, y = SCr)) +
-  geom_point(aes(color = as.factor(ID))) +
-  theme_bw()
-
-variables_of_interest <- c("Platelets", "SCr", "Albumin", "Bilirubin", "Lymphocytes", "Neutrophils")
-plot_explore <- data_head %>% 
-  select(all_of(c("ID", "gest", variables_of_interest))) %>% 
-  pivot_longer(-c(ID, gest), names_to = "variable") %>% 
-  ggplot(aes(x = gest, y = value, group = as.factor(ID), color = as.factor(ID))) +
-  geom_line() +
-  facet_wrap(~ variable, ncol = 1, scales = "free_y", strip.position = "right") + 
-  theme_bw()
 
 #Data preparation: Remove (most) imputed values
 data_reduced <- data_pregnancy_raw
@@ -42,10 +27,8 @@ for (i in 2:nrow(data_reduced)) {
   if (all(current_meas == previous_meas)) {
     remove_rows <- c(remove_rows, i)
   }
-  
   previous_meas <- current_meas
 }
-
 data_reduced <- data_reduced[-remove_rows, ]
 
 
@@ -56,62 +39,10 @@ write.csv(data_reduced, file = "data/clean/pregnancy_reduced.csv", row.names = F
 #continue with data_reduced
 data_reduced <- read.csv("data/clean/pregnancy_reduced.csv", row.names = NULL)
 
-#plot data
-variables_of_interest <- c("Platelets", "SCr", "Albumin", "Bilirubin", "Lymphocytes", "Neutrophils")
-plot_explore <- data_reduced %>% 
-  select(all_of(c("ID", "gest", variables_of_interest))) %>% 
-  pivot_longer(-c(ID, gest), names_to = "variable") %>% 
-  ggplot(aes(x = gest, y = value)) +
-  geom_line(aes(group = as.factor(ID)), color = "grey35") +
-  geom_smooth(color = "red", se = F) +
-  facet_wrap(~ variable, ncol = 1, scales = "free_y", strip.position = "right") + 
-  theme_bw()
-
-#select one variable for over time copula: Albumin
-#plot Albumin
-plot_Albumin_orig <- data_pregnancy_raw %>% 
-  ggplot(aes(x = gest, y = Albumin)) +
-  geom_line(aes(group = as.factor(ID)), color = "grey35") +
-  geom_smooth(color = "red", se = F) +
-  theme_bw()
-
-plot_Albumin <- data_reduced %>% 
-  ggplot(aes(x = gest, y = Albumin)) +
-  geom_line(aes(group = as.factor(ID)), color = "grey35") +
-  geom_smooth(color = "red", se = F) +
-  theme_bw()
-
-pdf("results/figures/Albumin_over_time.pdf", width = 5, height = 4)
-plot_Albumin_orig
-plot_Albumin
-dev.off()
-
 ### Using polynomials
 #remove all observations with baby (only looking at pregnancy)
 data_clean <- data_reduced %>% 
   filter(BABY == 0)
-
-
-#visualize polynomials for subset
-data_clean %>% 
-  filter(ID %in% c(1:3, 5:13)) %>%
-  ggplot(aes(x = gest, y = Albumin)) +
-  geom_point() +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = F) +
-  facet_wrap(~ ID) +
-  theme_bw()
-
-fixed_lm_poly <- lm(data = data_clean, Albumin ~ poly(gest, 2, raw = TRUE))
-
-fixed_lm_function <- function(x, lm_obj) {
-  coefs <- coef(lm_obj)
-  y <- coefs[1] + coefs[2]*x + coefs[3]*x^2
-  return(y)
-}
-
-with(data_clean, plot(Albumin ~ gest))
-lines(fixed_lm_function(sort(unique(data_clean$gest)), fixed_lm_poly) ~ sort(unique(data_clean$gest)), col = "red")
-
 
 #mixed effect model
 re_lm <- lmer(data = data_clean,
@@ -120,37 +51,7 @@ sum_model <- summary(re_lm)
 individual_coefs <- coef(re_lm)$ID
 names(individual_coefs) <- paste0("b", 0:2)
 
-#plot results
-poly_df <- individual_coefs %>% 
-  rownames_to_column("ID") %>%
-  mutate(ID = as.integer(ID)) %>%
-  right_join(data_clean %>% select(ID, Albumin, gest)) %>% 
-  mutate(pred =  b0 + b1*gest + b2*gest^2) %>% 
-  mutate(res = Albumin - pred)
-
-
-poly_df %>% 
-  filter(ID %in% 1:12) %>% 
-  ggplot(aes(x = gest)) +
-  geom_line(aes(y = pred), color = "red") +
-  geom_point(aes(y = Albumin)) +
-  facet_wrap(~ ID) +
-  theme_bw()
-
-pdf("results/figures/longitudinal_alb_polynomials.pdf", width = 20, height = 20)
-print(poly_df %>% 
-        ggplot(aes(x = gest)) +
-        geom_line(aes(y = pred), color = "red") +
-        geom_point(aes(y = Albumin)) +
-        facet_wrap(~ ID) +
-        theme_bw())
-dev.off()
-
 #fit copula curves
-
-hist(individual_coefs$b0)
-hist(individual_coefs$b1)
-hist(individual_coefs$b2)
 marg_beta0 <- estimate_parametric_marginal(individual_coefs$b0, "norm")
 marg_beta1 <- estimate_parametric_marginal(individual_coefs$b1, "norm")
 marg_beta2 <- estimate_parametric_marginal(individual_coefs$b2, "norm")
@@ -159,9 +60,6 @@ uniform_coefs <- data.frame(b0 = marg_beta0$pit(individual_coefs$b0),
                             b1 = marg_beta1$pit(individual_coefs$b1),
                             b2 = marg_beta2$pit(individual_coefs$b2))
 vine_coefs <- vinecop(uniform_coefs)
-contour(vine_coefs)
-pairs_copula_data(uniform_coefs)
-cor(uniform_coefs, method = "kendall")
 
 pdf("results/figures/longitudinal_parameter_copula.pdf")
 contour(vine_coefs)
@@ -196,10 +94,10 @@ poly_df_sim %>%
 
 ## create copula for gest and residual noise
 poly_df %>% 
-  ggplot(aes(x = gest, y = res)) +
-  geom_point() +
+  ggplot(aes(x = gest, y = res, color = as.factor(ID))) +
+  geom_point(show.legend = F) +
+  geom_line(aes(group = as.factor(ID)), show.legend = F) +
   theme_bw()
-hist(poly_df$gest)
 marg_gest <- estimate_spline_marginal(poly_df$gest)
 check_fit_plot(poly_df$gest, marg_gest$density)
 
@@ -274,3 +172,118 @@ plot_simulated_curves <- poly_df %>%
 pdf("results/figures/longitudinal_alb_polynomials_both.pdf", width = 6, height = 4)
 print(plot_simulated_curves)
 dev.off()
+
+
+
+#Polynomials for multiple variables
+variables_of_interest <- c("Platelets", "SCr", "Albumin", "Bilirubin", "Lymphocytes", "Neutrophils")
+
+coef_data_raw <- as.data.frame(matrix(NA, nrow = length(unique(data_clean$ID)), ncol = 3*length(variables_of_interest)))
+names(coef_data_raw) <- paste0("b", 0:2, "_", rep(variables_of_interest, each = 3))
+
+for (variable in variables_of_interest) {
+  formula_v <- as.formula(paste(variable, "~ poly(gest, 2, raw = TRUE) + (1 + poly(gest,2, raw = TRUE)|ID)"))
+  
+  re_lm <- lmer(data = data_clean, formula_v, REML = TRUE)
+  individual_coefs <- coef(re_lm)$ID
+  
+  coef_data_raw[grep(paste0("_", variable), names(coef_data_raw))] <-  coef(re_lm)$ID
+}
+
+coef_data_unif <- coef_data_raw
+marginals <- list()
+for (variable in names(coef_data_unif)) {
+  marginals[[variable]] <- estimate_spline_marginal(coef_data_unif[, variable])
+  ind_na <- is.na(coef_data_unif[, variable])
+  coef_data_unif[!ind_na, variable] <- marginals[[variable]]$pit(coef_data_unif[!ind_na, variable])
+}
+
+
+vine_coefs <- vinecop(coef_data_unif)
+plot(vine_coefs, var_names = "use", tree = 1:2)
+pdf("results/figures/longitudinal_parameter_copula.pdf", width = 20, height = 20)
+contour(vine_coefs)
+dev.off()
+
+#Simulation
+set.seed(123)
+m <- 123
+sim_unif <- as.data.frame(rvinecop(n = m, vine_coefs))
+
+sim_data <- sim_unif
+for (variable in names(sim_data)) {
+  ind_na <- is.na(sim_data[, variable])
+  sim_data[!ind_na, variable] <- marginals[[variable]]$pdf(sim_data[!ind_na, variable])
+}
+
+gest_times <- seq(min(data_clean$gest), max(data_clean$gest), by = 1/7)
+df_sim <- sim_data %>% 
+  rownames_to_column("ID") %>%
+  right_join(as.data.frame(expand_grid(gest = gest_times, ID = rownames(sim_data))))
+
+for (variable in variables_of_interest) {
+  col_ind <- grep(paste0("_", variable), names(df_sim))
+  df_sim[, variable] <- df_sim[, col_ind[1]] + df_sim[, col_ind[2]]*df_sim$gest + df_sim[, col_ind[3]]*df_sim$gest^2
+}
+
+
+df_poly <- coef_data_raw %>% 
+  rownames_to_column("ID") %>%
+  right_join(as.data.frame(expand_grid(gest = gest_times, ID = rownames(coef_data_raw))))
+
+for (variable in variables_of_interest) {
+  col_ind <- grep(paste0("_", variable), names(df_poly))
+  df_poly[, variable] <- df_poly[, col_ind[1]] + df_poly[, col_ind[2]]*df_poly$gest + df_poly[, col_ind[3]]*df_poly$gest^2
+}
+
+#Plotting results
+#observed data
+plot_explore <- data_clean %>% 
+  select(all_of(c("ID", "gest", variables_of_interest))) %>% 
+  pivot_longer(-c(ID, gest), names_to = "variable") %>% 
+  ggplot(aes(x = gest, y = value)) +
+  geom_line(aes(group = as.factor(ID)), color = "grey35") +
+  geom_smooth(color = "red", se = F) +
+  facet_wrap(~ variable, ncol = 1, scales = "free_y", strip.position = "right") + 
+  theme_bw()
+
+#simulated polynomials
+plot_sim <- df_sim %>% 
+  select(all_of(c("ID", "gest", variables_of_interest))) %>% 
+  pivot_longer(-c(ID, gest), names_to = "variable") %>% 
+  ggplot(aes(x = gest, y = value)) +
+  geom_line(aes(group = as.factor(ID)), color = "grey35") +
+  geom_smooth(color = "red", se = F) +
+  facet_wrap(~ variable, ncol = 1, scales = "free_y", strip.position = "right") + 
+  theme_bw()
+
+#estimated polynomials from observed data
+plot_curves <- df_poly %>% 
+  select(all_of(c("ID", "gest", variables_of_interest))) %>% 
+  pivot_longer(-c(ID, gest), names_to = "variable") %>% 
+  ggplot(aes(x = gest, y = value)) +
+  geom_line(aes(group = as.factor(ID)), color = "grey35") +
+  geom_smooth(color = "red", se = F) +
+  facet_wrap(~ variable, ncol = 1, scales = "free_y", strip.position = "right") + 
+  theme_bw()
+
+
+#side-by-side comparison of the estimated and simulated curves
+df_sim %>% 
+  mutate(type = "simulation") %>% 
+  bind_rows(df_poly %>% mutate(type = "observed")) %>% 
+  select(all_of(c("ID", "gest", variables_of_interest, "type"))) %>% 
+  pivot_longer(-c(ID, gest, type), names_to = "variable") %>% 
+  ggplot(aes(x = gest, y = value)) +
+  geom_line(aes(group = as.factor(ID)), color = "grey35") +
+  geom_smooth(color = "red", se = F) +
+  facet_grid(variable ~ type, scales = "free_y") + 
+  theme_bw()
+
+
+#correlations at different timepoints
+cov(df_sim %>% filter(gest == gest_times[10]) %>% select(variables_of_interest))
+cov(df_poly %>% filter(gest == gest_times[10]) %>% select(variables_of_interest))
+
+
+gest_times
---
+++ scripts/longitudinal_copulas_plots.R
@@ -0,0 +1,126 @@
+#Longitudinal analysis plotting
+library(fitdistrplus)
+library(tidyverse)
+library(rvinecopulib)
+library(actuar)
+library(EnvStats)
+library(mvtnorm)
+library(kde1d)
+library(lme4)
+
+source("scripts/functions/functions.R")
+#read in pregnancy data
+data_pregnancy_raw <- read.csv("data/Jig - pregnancy file August 2012 minus coag info.csv", 
+                               row.names = NULL, na.strings = c("")) %>% 
+  mutate(Neutrophils = as.numeric(str_replace(Neutrophils, ":", ".")))
+data_reduced <- read.csv("data/clean/pregnancy_reduced.csv", row.names = NULL)
+
+data_clean <- data_reduced %>% 
+  filter(BABY == 0)
+
+#explore some patients
+data_head <- data_pregnancy_raw %>% filter(ID %in% 1:5)
+data_head %>% 
+  ggplot(aes(x = Platelets, y = SCr)) +
+  geom_point(aes(color = as.factor(ID))) +
+  theme_bw()
+
+variables_of_interest <- c("Platelets", "SCr", "Albumin", "Bilirubin", "Lymphocytes", "Neutrophils")
+plot_explore <- data_head %>% 
+  select(all_of(c("ID", "gest", variables_of_interest))) %>% 
+  pivot_longer(-c(ID, gest), names_to = "variable") %>% 
+  ggplot(aes(x = gest, y = value, group = as.factor(ID), color = as.factor(ID))) +
+  geom_line() +
+  facet_wrap(~ variable, ncol = 1, scales = "free_y", strip.position = "right") + 
+  theme_bw()
+
+#plot reduced data - patient profiles
+variables_of_interest <- c("Platelets", "SCr", "Albumin", "Bilirubin", "Lymphocytes", "Neutrophils")
+plot_explore <- data_reduced %>% 
+  select(all_of(c("ID", "gest", variables_of_interest))) %>% 
+  pivot_longer(-c(ID, gest), names_to = "variable") %>% 
+  ggplot(aes(x = gest, y = value)) +
+  geom_line(aes(group = as.factor(ID)), color = "grey35") +
+  geom_smooth(color = "red", se = F) +
+  facet_wrap(~ variable, ncol = 1, scales = "free_y", strip.position = "right") + 
+  theme_bw()
+
+#select one variable for over time copula: Albumin
+#plot Albumin
+plot_Albumin_orig <- data_pregnancy_raw %>% 
+  ggplot(aes(x = gest, y = Albumin)) +
+  geom_line(aes(group = as.factor(ID)), color = "grey35") +
+  geom_smooth(color = "red", se = F) +
+  theme_bw()
+
+plot_Albumin <- data_reduced %>% 
+  ggplot(aes(x = gest, y = Albumin)) +
+  geom_line(aes(group = as.factor(ID)), color = "grey35") +
+  geom_smooth(color = "red", se = F) +
+  theme_bw()
+
+pdf("results/figures/Albumin_over_time.pdf", width = 5, height = 4)
+plot_Albumin_orig
+plot_Albumin
+dev.off()
+
+
+#visualize polynomials for subset
+data_clean %>% 
+  filter(ID %in% c(1:3, 5:13)) %>%
+  ggplot(aes(x = gest, y = Albumin)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = F) +
+  facet_wrap(~ ID) +
+  theme_bw()
+
+fixed_lm_function <- function(x, lm_obj) {
+  coefs <- coef(lm_obj)
+  y <- coefs[1] + coefs[2]*x + coefs[3]*x^2
+  return(y)
+}
+
+with(data_clean, plot(Albumin ~ gest))
+lines(fixed_lm_function(sort(unique(data_clean$gest)), fixed_lm_poly) ~ sort(unique(data_clean$gest)), col = "red")
+
+
+#individual polynomial predictions:
+#mixed effect model
+re_lm <- lmer(data = data_clean,
+              Albumin ~ poly(gest, 2, raw = TRUE) + (1 + poly(gest,2, raw = TRUE)|ID), REML = TRUE)
+sum_model <- summary(re_lm)
+individual_coefs <- coef(re_lm)$ID
+names(individual_coefs) <- paste0("b", 0:2)
+
+#plot results
+poly_df <- individual_coefs %>% 
+  rownames_to_column("ID") %>%
+  mutate(ID = as.integer(ID)) %>%
+  right_join(data_clean %>% select(ID, Albumin, gest)) %>% 
+  mutate(pred =  b0 + b1*gest + b2*gest^2) %>% 
+  mutate(res = Albumin - pred)
+
+
+poly_df %>% 
+  filter(ID %in% 1:12) %>% 
+  ggplot(aes(x = gest)) +
+  geom_line(aes(y = pred), color = "red") +
+  geom_point(aes(y = Albumin)) +
+  facet_wrap(~ ID) +
+  theme_bw()
+
+pdf("results/figures/longitudinal_alb_polynomials.pdf", width = 20, height = 20)
+print(poly_df %>% 
+        ggplot(aes(x = gest)) +
+        geom_line(aes(y = pred), color = "red") +
+        geom_point(aes(y = Albumin)) +
+        facet_wrap(~ ID) +
+        theme_bw())
+dev.off()
+
+
+#copula
+
+hist(individual_coefs$b0)
+hist(individual_coefs$b1)
+hist(individual_coefs$b2)
